00921f11f51330e6249d39c0cc0c3da4ad1f46d9
Ryan.T <78358288+teffahiryan@users.noreply.github.com>
2023-05-30 13:52:37 +0200

    create exercise, request

--- database/migrations/2023_05_30_113637_create_exercises_table.php
+++ database/migrations/2023_05_30_113637_create_exercises_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('exercises', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->integer('repetition');
+            $table->integer('set');
             $table->timestamps();
         });
     }
